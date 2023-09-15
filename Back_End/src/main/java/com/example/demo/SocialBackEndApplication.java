11210a06ff82382659cffd065aadbedeca005173
imransuet <imransuet@gmail.com>
2023-09-15 08:21:27 +0200

    added Spring Boot

---
+++ Back_End/src/main/java/com/example/demo/SocialBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBackEndApplication.class, args);
+	}
+
+}

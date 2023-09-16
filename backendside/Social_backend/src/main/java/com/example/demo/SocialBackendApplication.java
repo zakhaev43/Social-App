2e54e263f064192669aa078e9152e2df95d4f6a2
imransuet <imransuet@gmail.com>
2023-09-16 20:06:59 +0200

    added test Controller

--- backendside/Social_backend/src/main/java/com/example/demo/SocialBackendApplication.java
+++ backendside/Social_backend/src/main/java/com/example/demo/SocialBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialBackEndApplication {
+public class SocialBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialBackEndApplication.class, args);
+		SpringApplication.run(SocialBackendApplication.class, args);
 	}
 
 }
